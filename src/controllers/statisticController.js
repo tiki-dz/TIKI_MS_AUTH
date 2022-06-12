5f41d9158e1af2da1a40454d25d25731b51c813a
ali1sba <hariri.ali.official@gmail.com>
2022-06-12 21:56:35 +0100

    stat partie

---
+++ src/controllers/statisticController.js
@@ -0,0 +1,78 @@
+const { User, Client, Account, Partner } = require('../models')
+const { STATISTIC_BINDING_KEY } = require('../config/config.js')
+const rabbitMq = require('../utils')
+
+async function update (req, res) {
+  try {
+    const totalClient = await Client.count()
+    const manClient = await User.count({
+      where: { type: 'client', sexe: 1 }
+    })
+    const womenClient = await User.count({
+      where: { type: 'client', sexe: 0 }
+    })
+    const deactivatedClient = await User.count({
+      where: { type: 'client' },
+      include: [
+        {
+          model: Account,
+          attributes: [],
+          where: {
+            state: 0
+          }
+        }
+      ]
+    })
+    const activatedClient = await User.count({
+      where: { type: 'client' },
+      include: [
+        {
+          model: Account,
+          attributes: [],
+          where: {
+            state: 1
+          }
+        }
+      ]
+    })
+
+    const totalPartner = await Partner.count()
+
+    const deactivatedPartner = await User.count({
+      where: { type: 'partner' },
+      include: [
+        {
+          model: Account,
+          attributes: [],
+          where: {
+            state: 0
+          }
+        }
+      ]
+    })
+    const activatedPartner = await User.count({
+      where: { type: 'partner' },
+      include: [
+        {
+          model: Account,
+          attributes: [],
+          where: {
+            state: 1
+          }
+        }
+      ]
+    })
+
+    const channel = rabbitMq.channel
+    const payload = { totalClient: totalClient, manClient: manClient, womenClient: womenClient, activatedClient: activatedClient, deactivatedClient: deactivatedClient, totalPartner: totalPartner, deactivatedPartner: deactivatedPartner, activatedPartner: activatedPartner }
+    const message = [{ event: 'UPDATE-USER-STAT', payload: payload }]
+    rabbitMq.PublishMessage(channel, STATISTIC_BINDING_KEY, message)
+    console.log('published new event')
+
+    return res.status(200).send({ success: true, message: 'success' })
+  } catch (error) {
+    return res.status(500).send({ error: error, success: false, message: 'processing err' })
+  }
+}
+
+module.exports = { update }
